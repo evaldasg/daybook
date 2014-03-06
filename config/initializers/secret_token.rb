54ca5c9a273ec43e257154ba57ac41756abe8ad0
Evaldas Grudzinskas <evaldas.g@icloud.com>
2014-03-06 13:45:08 +0200

    Initial Rails 4.0.3 setup with Foundation 5.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Daybook::Application.config.secret_key_base = 'b66b556a9cbe69e54d86656d083188da4b4b13595244f3f8c1aadc3a70885004fbc196cd43c523f5adb75aad4af6ba4880235f8b6ac15ece60dbce615b011c33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Daybook::Application.config.session_store :cookie_store, key: '_daybook_session'
