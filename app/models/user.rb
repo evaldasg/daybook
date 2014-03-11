564f0fed38c1071ad8780de390d877ad07ef3473
Evaldas Grudzinskas <evaldas.g@icloud.com>
2014-03-11 07:29:00 +0200

    Created [users] UI with zurb foundation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :name, :username, :email, presence: true
   validates :email, :username, uniqueness: { case_sensitive: false }
+  validates :username, format: /\A[A-Z0-9]+\z/i
   validates :email, format: /\A\S+@\S+\z/
-  validates :password, length: { minimum: 6, allow_balnk: true }
+  validates :password, length: { minimum: 6, allow_blank: true }
 
 end
---
+++ spec/features/list_users_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe "Viewing the list of users" do
+
+  it "shows the users" do
+    user1 = create(:user)
+    user2 = create(:user)
+    user3 = create(:user)
+
+    visit users_url
+
+    expect(page).to have_link(user1.name)
+    expect(page).to have_link(user2.name)
+    expect(page).to have_link(user3.name)
+  end
+
+end
